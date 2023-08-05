a1dedc971fc2def01c976d6b8b8f7e725478a8c9
vinia <viniciusadams98@gmail.com>
2023-08-05 18:33:29 +0100

    adding heading constant

--- src/App.js
+++ src/App.js
@@ -4,9 +4,11 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import MovieList from './components/MovieList.js';
 
 import './App.css';
+import MovieListHeading from './components/MovieListHeading.js';
 
 const App = () => {
-  const [movies, setMovies] = useState([])
+  const [movies, setMovies] = useState([]);
+  const [search , setSearchValue] = useState ('')
   const getMovieRequest = async() => {
    const url = "https://www.omdbapi.com/?s=avengers&apikey=9d31e322"
     const response = await fetch (url);
@@ -21,6 +23,9 @@ const App = () => {
   
   return (
     <div className= 'container-fluid movie-app'>
+      <div className='row'>
+      <MovieListHeading heading= 'Movies'/>
+      </div>
       <div className='row'>
       <MovieList movies={movies} />
     </div>
